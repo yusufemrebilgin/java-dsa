e4353b42b8379eebebbb576ebe9efdb833125d22
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-03-23 16:16:01 +0300

    adding util class

---
+++ src/common/util/DataSupplier.java
@@ -0,0 +1,53 @@
+package common.util;
+
+import common.model.Employee;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+
+public class DataSupplier {
+
+    // hard coded values for generating random instances
+    private static final String[] FIRST_NAMES = {
+            "Emily", "Alexander", "Sophia", "Benjamin", "Isabella", "William",
+            "Olivia", "Noah", "Emma", "Liam"
+    };
+
+    private static final String[] LAST_NAMES = {
+            "Smith", "Johnson", "Brown", "Lee", "Garcia", "Martinez", "Taylor",
+            "Wilson", "Anderson", "Thomas"
+    };
+
+    private DataSupplier() {}
+
+    public static List<Employee> generateRandomEmployees(int numOfEmployees) {
+        List<Employee> employees = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < numOfEmployees; i++) {
+            int id = random.nextInt(numOfEmployees * 10);
+            String firstName = FIRST_NAMES[random.nextInt(FIRST_NAMES.length)];
+            String lastName = LAST_NAMES[random.nextInt(LAST_NAMES.length)];
+            String email = firstName.toLowerCase() + lastName.toLowerCase() + "@example.com";
+            employees.add(new Employee(id, firstName, lastName, email));
+        }
+        return employees;
+    }
+
+    public static Comparator<Employee> getEmployeeComparatorBy(final String compareBy) {
+
+        return switch (compareBy.toLowerCase()) {
+            case "firstname" -> Comparator.comparing(Employee::getFirstName);
+            case "lastname" -> Comparator.comparing(Employee::getLastName);
+            case "email" -> Comparator.comparing(Employee::getEmail);
+            case "all" -> Comparator.comparing(Employee::getFirstName)
+                    .thenComparing(Employee::getLastName)
+                    .thenComparing(Employee::getEmail);
+            default -> throw new IllegalArgumentException("Invalid field for comparison: " + compareBy);
+        };
+
+    }
+
+}
